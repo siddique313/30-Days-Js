fefe76f22c52bd03baeaa45760e4676fdbd01b37
siddique313 <siddiqueali531@gmail.com>
2024-07-15 10:40:54 +0000

    container config done

---
+++ Day02.js
@@ -0,0 +1,81 @@
+// 1. Task 1: Write a program to add two numbers and log the result to the console.
+
+const num1 = 3
+const num2 = 3
+// console.log(num1 + num2);
+
+// 1. Task 2: Write a program to Sub two numbers and log the result to the console.
+
+let sub1 = 3
+let sub = 3
+console.log(sub1 - sub);
+
+// 1. Task 3: Write a program to multiply two numbers and log the result to the console.
+
+const multiply = 3
+console.log(multiply * 4);
+// 1. Task 4: Write a program to divide two numbers and log the result to the console.
+
+const divide = 3
+console.log(divide / 2);
+
+
+// 1. Task 5: Write a program to find a reminder when one numbe is divided by another and log the result to the console
+
+
+let mode = 4
+let mode1 = 2
+console.log(mode% mode1);
+
+
+// Activity 2: Assignnment Operators
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let op =  23
+op+= 12
+console.log(op);
+
+// Task 7 Use the -= operator to sub a number to a variable and log the result to the console.
+
+let op1 = 23
+op1 -= 12
+console.log(op1);
+
+// Activity 3 Comparison Operator
+    // Task 8: Write a program to compare two number using > and < and log the result to the console.
+
+let cmp = 10 < 12
+console.log(cmp);
+let cmp1 = 10 > 12
+console.log(cmp1);
+
+ // Task 9: Write a program to compared two number using >= and <= and log the result to the consoele.
+
+let cmpeqOp = 101 <= 12
+console.log(cmpeqOp);
+let cmpeqOp1 = 10 >= 10
+console.log(cmpeqOp1);
+
+ // Task 10: Write a program to compare two numbers using == and === and the result to the console.
+
+ let compOp = 10 ==10
+ console.log(compOp);
+let cmpop1 = 10 === 21
+console.log(cmpop1);
+// Activity 4: Logical Operator
+    // Task 11: Write a program that uses the && operators to combine two conditions and log the result to the conosle.
+
+    let operat = 'h' && ''
+    console.log(operat);
+    
+     // Task 12: Write a program that uses the || operators to combine two conditions and log the result to the conosle.
+
+     let operat1 = 'hello' || null
+     console.log(operat1);
+ 
+
+// Activity 5: Ternary Operator
+// Task 14: Write a program that uses the ternary operator to check if a  number is positive or negative and log the result to the console.
+
+let ternaryOp = 12 < 0 ? 'negative' : 'Positive'
+console.log(ternaryOp);
